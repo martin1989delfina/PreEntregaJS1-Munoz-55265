b6e7f5ba3479a2a866bfc57e12c679d26cda18cf
martin1989 <gimenamartinjosefinamia@gmail.com>
2023-07-09 21:18:07 -0300

    Correccion isNan

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let calcular = prompt("Ingresá que valor querés calcular ---DOLARES O EUROS---
 function dolarBlue() {
     const precioDolar = 492;
     let pesosUsuario = parseInt(prompt("Ingresá la cantidad de Pesos a convertir"))
-    if (pesosUsuario = isNaN) {
+    if (isNaN(pesosUsuario)) {
         alert("Ingresá un valor válido")
     } else {
         alert("Son $" + (pesosUsuario / precioDolar).toFixed(2) + " Dolares Blue")
@@ -19,7 +19,7 @@ while (calcular != "STOP") {
             alert("Ingesaste EUROS");
             let euroBlue = 535
             let pesosUsuario2 = parseInt(prompt("Ingresá la cantidad de Pesos a convertir"))
-            if (pesosUsuario2 = isNaN) {
+            if (isNaN(pesosUsuario2)) {
                 alert("Ingresá un valor válido")
             } else {
                 alert("Son $" + (pesosUsuario2 / euroBlue).toFixed(2) + " Euros Blue")
